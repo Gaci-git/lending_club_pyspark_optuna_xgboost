223dd28e39875c7728e3d6e1b0a65cb4667acdaf
Gaci-git <69762251+Gaci-git@users.noreply.github.com>
2022-11-13 21:19:27 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,11 +12,12 @@ st.markdown(
     Machine Learning and Data Science projects.
     **👈 Select a page from the sidebar** to see Machine Learning models
     for Loan Prediction!
+    
     ### Want kind of models this app contains?
     - Loan Outcome prediction model
     - Loan Grading prediction model
     - Loan Sub-grading prediction model
-        forums](https://discuss.streamlit.io)
+
     ### See full code here:
     - Git hub repository for [Loan_club](https://github.com/Gaci-git/loan_club)
 """
