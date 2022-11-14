e52512817dd16a2d29e675d6cb1faf02ccbe00ef
Gaci-git <69762251+Gaci-git@users.noreply.github.com>
2022-11-14 20:05:08 +0200

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -5,7 +5,7 @@ import numpy as np
 @st.cache(suppress_st_warning=True)
 
 
-model = loaded_model = tf.keras.models.load_model('my_model')
+model = tf.keras.models.load_model('my_model')
 
 # Define the prediction function
 def predict(loan_amnt, term,
