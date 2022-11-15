47b06e69f9318cb3327081bdf5352e7cedd6bb3f
Gaci-git <69762251+Gaci-git@users.noreply.github.com>
2022-11-15 16:36:34 +0200

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import tensorflow as tf
 
-from tensorflow import keras from tensorflow.keras import layers
+from tensorflow import keras 
+from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import numpy as np
 
